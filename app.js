156cbb6d78af5e39f190ab95536e3ef3e7f8db76
Ross Wu <RAWSE-DA-BAWSE@Rosss-MacBook-Pro.local>
2017-06-10 16:44:07 -1000

    working on films

--- app.js
+++ app.js
@@ -10,19 +10,32 @@
         person4Name.innerHTML = response.name;
     }
 
-    function getstarWarsId(id){
+    function getstarWarsName(id){
         oReq.addEventListener('load', starWarsName);
         oReq.open('GET', `http://www.swapi.co/api/people/${id}`); 
         oReq.send();
     }
-    function getPlanet(){
-
-    }
 
     var oReq2 = new XMLHttpRequest();
 
+    function starWarsPlanet(){
+        var person4 = document.querySelector('person4');
+        var response = JSON.parse(this.responseText);
+        console.log(response);
+
+        person4HomeWorld.innerHTML = response.name;
+    }
+
+    function getstarWarsPlanet(id){
+        oReq2.addEventListener('load', starWarsPlanet);
+        oReq2.open('GET', `http://www.swapi.co/api/planets/${id}`); 
+        oReq2.send();
+    }
+
+    var oReq3 = new XMLHttpRequest();
+
     function newStarWarsName(){
-        var contain14 = document.querySelector('person14');
+        var person14 = document.querySelector('person14');
         var response = JSON.parse(this.responseText);
         console.log(response);
 
@@ -30,15 +43,66 @@
     }
 
     function getNewStarWarsName(id){
-        oReq2.addEventListener('load', newStarWarsName);
-        oReq2.open('GET', `http://www.swapi.co/api/people/${id}`);
-        oReq2.send();
-    
+        oReq3.addEventListener('load', newStarWarsName);
+        oReq3.open('GET', `http://www.swapi.co/api/people/${id}`);
+        oReq3.send();
     }
+
+    var oReq4 = new XMLHttpRequest();
+
+    function starWarsSpecies(){
+      var person14 = document.querySelector('person14');
+      var response = JSON.parse(this.response);
+      console.log(response);
+
+      person14Species.innerHTML = response.name;
+    }
+
+    function getstarWarsSpecies(id){
+      oReq4.addEventListener('load',starWarsSpecies);
+      oReq4.open('GET', `http://www.swapi.co/api/species/${id}`);
+      oReq4.send();
+    }
+
+     var oReq5 = new XMLHttpRequest()
+    
+    function getFilms(){  
+
+      for (var i = 0, len = res.results.length; i < len; i++) {
+        
+        filmResult = len[i];
+
+    var filmPlanetsContainer = document.createElement("ul");
+      filmPlanetsContainer.className = "filmPlanets";
+      filmPlanetsContainer.innerHTML = filmResult.planets;
+
+    for (var p = 0; p < res.results[i].planets.length; p++) {
+      planetResult = res.results[i];
+
+      (function(myPlanets) {
+        var pReq = new XMLHttpRequest();
+        pReq.addEventListener("load", function(){
+          var pRes = JSON.parse(this.responseText);
+
+          var planetListItemContainer = document.createElement("li");
+            planetListItemContainer.className = "planets";
+            planetListItemContainer.innerHTML = planetResult[i];
+
+          myPlanets.appendChild(planetListItemContainer);
+        });
+
+        oReq5.open("GET", `http://swapi.co/api/films/`,res.results[i].planets[p]);
+        oReq5.send();
+
+      })(filmPlanetsContainer);
+
+    }
+  }
+}
   
-
-
-
-getstarWarsId(4);
+getstarWarsName(4);
+getstarWarsPlanet(1);
 getNewStarWarsName(14);
+getstarWarsSpecies(1);
+getFilms();
 })();
